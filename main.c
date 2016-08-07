e72b864a59171ebd4376662c4f280e875e6fc9df
U-DESKTOP-VI3ELN4\bata <tanacchi>
2016-08-07 15:55:02 +0900

    complete -c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int kansuu(int x);
 
@@ -6,9 +7,9 @@ void print(void);
 
 int command_num(int i, char src[]);
 
-int main(int argc, char* argv[]){
+void multiple_ary(void);
 
-  int command_num = -1;
+int main(int argc, char* argv[]){
   
   if (argc < 2){
     printf(
@@ -18,7 +19,20 @@ int main(int argc, char* argv[]){
   }
   int i, command;
   for (i = 1; i < argc; i++){
-    command = command_num(i, argv);
+
+    if      (!(strcmp(argv[i], "-a"))) command = 0;
+    else if (!(strcmp(argv[i], "-b"))) command = 1;
+    else if (!(strcmp(argv[i], "-c"))) command = 2;
+    else if (!(strcmp(argv[i], "-d"))) command = 3;
+    else if (!(strcmp(argv[i], "-e"))) command = 4;
+    else if (!(strcmp(argv[i], "-f"))) command = 5;
+    else if (!(strcmp(argv[i], "-g"))) command = 6;
+    else if (!(strcmp(argv[i], "-h"))) command = 7;
+    else if (!(strcmp(argv[i], "-i"))) command = 8;
+    else if (!(strcmp(argv[i], "-j"))) command = 9;
+    else if (!(strcmp(argv[i], "-k"))) command = 10;
+    else command = -1;
+
 
     switch (command){
     case -1:
@@ -31,6 +45,8 @@ int main(int argc, char* argv[]){
       print();
       break;
     case 2:
+      multiple_ary();
+      break;
     case 3:
     case 4:
     case 5:
@@ -47,9 +63,6 @@ int main(int argc, char* argv[]){
   return 0;
 }
 
-
-
-
 int kansuu(int x){
   int y;
   y = x * 6 + 20;
@@ -62,21 +75,23 @@ void print(void){
   printf("thank you (^ ^)\n");
   
 }
-
-int command_num(int i, char src[]){ 
-
-    if      (!(strcmp(src[i], "-a"))) return 0;
-    else if (!(strcmp(src[i], "-b"))) return 1;
-    else if (!(strcmp(src[i], "-c"))) return 2;
-    else if (!(strcmp(src[i], "-d"))) return 3;
-    else if (!(strcmp(src[i], "-e"))) return 4;
-    else if (!(strcmp(src[i], "-f"))) return 5;
-    else if (!(strcmp(src[i], "-g"))) return 6;
-    else if (!(strcmp(src[i], "-h"))) return 7;
-    else if (!(strcmp(src[i], "-i"))) return 8;
-    else if (!(strcmp(src[i], "-j"))) return 9;
-    else if (!(strcmp(src[i], "-k"))) return 10;
-    else                              return -1;
-
-}
     
+void multiple_ary(void){
+
+char* plate[3][5] = {		/* 4x6 pointer array*/
+    {"Rock",    "r", "1", "gu-",   "ROCK"},
+    {"scissors","s", "2", "choki", "SCISSORS"},
+    {"paper",   "p", "3", "pa-",   "PAPER"},
+};
+
+  int i, j, k;
+  for (i = 0; i < 3; i++){
+    for (j = 0; j < 5; j++){
+      /* for (k = 0; k < 10; k++){ */
+  	printf("%s"/* "%c" */, plate);
+      /* } */
+      putchar('\t');
+    }
+    putchar('\n');
+  }  
+}
