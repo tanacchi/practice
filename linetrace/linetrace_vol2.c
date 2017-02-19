071a3c1af014f74e46403bd29d9c4ae8a2888985
tanacchi <q111026d@mail.kyutech.jp>
2017-02-20 00:59:00 +0900

    Add linetrace set
    新入生教育と自身のスキル向上のため　また新たにリポジトリを作る可能性あり

---
+++ linetrace/linetrace_vol2.c
@@ -0,0 +1,12 @@
+#include "lpc13xx.h"
+#include "gpio.h"
+#include "vs-wrc103.h"
+#include "ixbus.h"
+
+
+//   const unsigned short MainCycle = 60;
+//  Init(MainCycle);
+
+//   Mtr_Run_lv(right, -left, 0, 0, 0, 0);
+
+// ADRead(ch) < THRESHOLD  <- white
