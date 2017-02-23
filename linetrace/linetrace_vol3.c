61ae6d78f1c998e8cb6868a3019cc5d673ca1bc6
tanacchi <q111026d@mail.kyutech.jp>
2017-02-23 16:27:03 +0900

    Add linetrace_vol3.c
    Re:Revenge linetrace

---
+++ linetrace/linetrace_vol3.c
@@ -0,0 +1,22 @@
+#include "lpc13xx.h"
+#include "gpio.h"
+#include "vs-wrc103.h"
+#include "ixbus.h"
+
+int read_sensor(unsigned char ch) {
+  return (ADRead(ch) > 200) ? 1 : 0;
+}
+
+void run_motor(int left, int right) {
+  Mtr_Run_lv(right, left, 0, 0, 0, 0);
+}
+
+int judge_position() {
+  return (read_sensor(0) << 1 | read_sesor(1));
+}
+
+int main() {
+  while (1)
+    LED(judge_position());
+  return 0;
+}
