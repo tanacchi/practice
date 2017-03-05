72287e698f57b8bce19849fc9d0dd4664f9641a9
tanacchi <q111026d@mail.kyutech.jp>
2017-03-06 00:32:22 +0900

    Add sample (level2)

--- linetrace/level2_sample.c
+++ linetrace/level2_sample.c
@@ -3,10 +3,46 @@
 #include "vs-wrc103.h"
 #include "ixbus.h"
 
+int main() {
+  const unsigned short MainCycle = 60;
+  Init(MainCycle);
 
-//   const unsigned short MainCycle = 60;
-//  Init(MainCycle);
+  int i = 0;
+  
+  switch (i) {
+    
+  case 0:
+    if (ADRead(0) < 200 && ADRead(1) < 200) Mtr_Run_lv(10000, -10000, 0, 0, 0, 0);
+    if (ADRead(0) < 200 && ADRead(1) > 200) Mtr_Run_lv(    0, -10000, 0, 0, 0, 0);
+    if (ADRead(0) > 200 && ADRead(1) < 200) Mtr_Run_lv(10000,      0, 0, 0, 0, 0);
+    if (ADRead(0) > 200 && ADRead(1) > 200) i++;
+    Wait(1);
+    break;
+    
+  case 1:
+    if (ADRead(0) < 200 && ADRead(1) < 200) i++;
+    else Mtr_Run_lv(10000, -10000, 0, 0, 0, 0);
+    Wait(1);
+    break;
+    
+  case 2:
+    if (ADRead(0) < 200 && ADRead(1) < 200) Mtr_Run_lv(10000, -10000, 0, 0, 0, 0);
+    if (ADRead(0) < 200 && ADRead(1) > 200) Mtr_Run_lv(    0, -10000, 0, 0, 0, 0);
+    if (ADRead(0) > 200 && ADRead(1) < 200) Mtr_Run_lv(10000,      0, 0, 0, 0, 0);
+    if (ADRead(0) > 200 && ADRead(1) > 200) i++;
+    Wait(1);
+    break;
+    
+  case 3:
+    if (ADRead(0) < 200 && ADRead(1) < 200) i++;
+    else Mtr_Run_lv(10000, -10000, 0, 0, 0, 0);
+    Wait(1);
+    break;
+    
+  case 4:
+    Mtr_Run_lv(0, 0, 0, 0, 0, 0);
+    break;
 
-//   Mtr_Run_lv(right, -left, 0, 0, 0, 0);
-
-// ADRead(ch) < THRESHOLD  <- white
+  }
+  return 0;
+}
