c046c8b5db0eb5db51163cd55e97f86cf9ea35e4
tanacchi <q111026d@mail.kyutech.jp>
2017-03-08 12:35:28 +0900

    Add random play mode

--- gomoku_game.c
+++ gomoku_game.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define BOARD_SIZE  10
 
+// 選択したポジションがtureか否かを個々でやる必要あり
+
 enum MODE {
-  MODE_INIT,
   MODE_OP,
   MODE_DISP,
   MODE_INPUT,
+  MODE_RAND,
   MODE_SWITCH,
   MODE_JUDGE,
+  MODE_ASK,
   MODE_ED
 };
 
@@ -21,102 +27,137 @@ enum STONE {
 int task_disp(int board[BOARD_SIZE][BOARD_SIZE]);
 void init_board(int board[BOARD_SIZE][BOARD_SIZE]);
 int task_input(int board[BOARD_SIZE][BOARD_SIZE], int active_player);
-int task_init(int board[BOARD_SIZE][BOARD_SIZE], int* active_player);
+int task_op(int board[BOARD_SIZE][BOARD_SIZE], int* active_player);
 int task_switch(int* active_player);
-char* convert_stone_into_char(int stone);
+char* convert_num_into_char(int stone);
 int is_inside_board(int input_x, int input_y);
 int check_length(int board[BOARD_SIZE][BOARD_SIZE], int x, int y);
 int task_judge(int board[BOARD_SIZE][BOARD_SIZE]);
+int task_play_again();
+int task_rand(int board[BOARD_SIZE][BOARD_SIZE], int active_player);
 
 int main() {
 
-  int mode = MODE_INIT;
+  int mode = MODE_OP;
   int board[BOARD_SIZE][BOARD_SIZE];
-  int active_player = 1;
+  int active_player;
 
   while (1) {
     switch (mode) {
-      case MODE_INIT:
-        mode = task_init(board, &active_player);
-        break;
-      case MODE_DISP:
-        mode = task_disp(board);
-        break;
-      case MODE_INPUT:
-        mode = task_input(board, active_player);
-        break;
-      case MODE_SWITCH:
-        mode = task_switch(&active_player);
-        break;
-      case MODE_JUDGE:
-        mode = task_judge(board);
-        break;
-      case MODE_ED:
-        printf("See you~~\n");
-        return 0;
+    case MODE_OP:
+      mode = task_op(board, &active_player);
+      break;
+    case MODE_DISP:
+      mode = task_disp(board);
+      break;
+    case MODE_INPUT:
+      mode = task_input(board, active_player);
+      break;
+    case MODE_RAND:
+      mode = task_rand(board, active_player);
+      break;
+    case MODE_SWITCH:
+      mode = task_switch(&active_player);
+      break;
+    case MODE_JUDGE:
+      mode = task_judge(board);
+      break;
+    case MODE_ASK:
+      mode = task_play_again();
+      break;
+    case MODE_ED:
+      printf("See you~~\n");
+      return 0;
     }
   }
 } 
 
-int task_init(int board[BOARD_SIZE][BOARD_SIZE], int* active_player) {
+int task_op(int board[BOARD_SIZE][BOARD_SIZE], int* active_player) {
   *active_player = STONE_BLACK;
+  srand((unsigned)time(NULL));
   init_board(board);
   task_disp(board);
 
-  return MODE_INPUT;
+  return MODE_RAND;
 }
 
 int task_disp(int board[BOARD_SIZE][BOARD_SIZE]) {
   int i, j;
-
+  static int round;
+  if (round) printf("%d番手\n", round);
+  round++;
   printf("  ");  
   for (i = 0; i < BOARD_SIZE; i++) printf("%d ", i);
   putchar('\n');
 
   for (i = 0; i < BOARD_SIZE; i++) {
     printf("%d ", i);
-    for (j = 0; j < BOARD_SIZE; j++) {
-      printf("%s ", convert_stone_into_char(board[i][j]));
-    }
+    for (j = 0; j < BOARD_SIZE; j++) printf("%s ", convert_num_into_char(board[i][j]));
     putchar('\n');
   }
   putchar('\n');
+  
   return MODE_JUDGE;
 }
 
 int task_input(int board[BOARD_SIZE][BOARD_SIZE], int active_player) {
   int pos_x, pos_y;
-
+  
   printf("%s の番です。どこに置きますか？\n",
-      (active_player == STONE_BLACK) ? "●" : "○");
-  while (1) {
-    scanf("%d %d", &pos_y, &pos_x);
-    putchar('\n');
-    if (board[pos_x][pos_y] == STONE_SPACE && is_inside_board(pos_x, pos_y)) break;
-    else printf("不正な入力です。\n");
+         (active_player == STONE_BLACK) ? "●" : "○");
+  scanf("%d %d", &pos_x, &pos_y);
+  putchar('\n');
+  
+  if (board[pos_y][pos_x] == STONE_SPACE && is_inside_board(pos_x, pos_y)) {
+    board[pos_y][pos_x] = active_player;
+    return MODE_DISP;
   }
-  board[pos_x][pos_y] = active_player;
-  return MODE_DISP;
+  else {
+    printf("不正な入力です。\n");
+    return MODE_INPUT;
+  }
+}
+
+int task_rand(int board[BOARD_SIZE][BOARD_SIZE], int active_player) {
+  int pos_x = rand() % 10;
+  int pos_y = rand() % 10;
+  if (board[pos_y][pos_x] == STONE_SPACE && is_inside_board(pos_x, pos_y)) {
+    board[pos_y][pos_x] = active_player;
+    return MODE_DISP;
+  }
+  else return MODE_RAND;
 }
 
 int task_switch(int* active_player) {
   *active_player = (*active_player == STONE_BLACK) ? STONE_WHITE : STONE_BLACK;
-  return MODE_INPUT;
+  return MODE_RAND;
 }
 
 int task_judge(int board[BOARD_SIZE][BOARD_SIZE]) {
   int i, j, len_flag;
-  for (i = 0; i < BOARD_SIZE; i++) {
-    for (j = 0; j < BOARD_SIZE; j++) {
+  for (i = 0; i <= BOARD_SIZE - 5; i++) {
+    for (j = 0; j <= BOARD_SIZE - 5; j++) {
       if (board[i][j] == STONE_SPACE) continue;
       if (check_length(board, j, i)) {
         printf("%s の勝ちです。\n", (board[i][j] == STONE_BLACK) ? "●" : "○");
-        return MODE_ED;
+        return MODE_ASK;
       }
     }
   }
-  printf("foo\n");
   return MODE_SWITCH;
+}
+
+int task_play_again() {
+  char input[8] = {0};
+  
+  printf("プレイアゲイン？？(yes/no)\n"
+         ">");
+  scanf("%7s", input);
+  putchar('\n');
+  
+  if (!strcmp("yes", input))       return MODE_OP;
+  else if (!strcmp("no", input))   return MODE_ED;
+  else printf("不正な入力です\n"); return MODE_ASK;
 }
 
 void init_board(int board[BOARD_SIZE][BOARD_SIZE]) {
@@ -127,11 +168,11 @@ void init_board(int board[BOARD_SIZE][BOARD_SIZE]) {
 }
 
 
-char* convert_stone_into_char(int stone) {
+char* convert_num_into_char(int stone) {
   switch (stone) {
-    case STONE_SPACE: return " ";
-    case STONE_BLACK: return "●";
-    case STONE_WHITE: return "○";
+  case STONE_SPACE: return " ";
+  case STONE_BLACK: return "●";
+  case STONE_WHITE: return "○";
   }
 }
 
