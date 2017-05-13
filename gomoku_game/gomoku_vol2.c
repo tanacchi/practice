beccb9b507ca409c0133f236320c4d3598af3813
tanacchi <q111026d@mail.kyutech.jp>
2017-05-13 17:02:10 +0900

    Update vol2

--- gomoku_game/gomoku_vol2.c
+++ gomoku_game/gomoku_vol2.c
@@ -3,35 +3,32 @@
 #define BOARD_SIZE 8
 
 enum Stone {
+  SPACE,
   WHITE,
-  BLACK,
-  SPACE
+  BLACK
 };
 
-char convert_stone_to_char(int);
-void init_board(int board[][BOARD_SIZE]);
-void show_board(const int board[][BOARD_SIZE]);
-int is_available_position(int x, int y, const int board[][BOARD_SIZE]);
+char convert_stone_to_char(int stone);
+void init_board(int (*board)[BOARD_SIZE]);
+void show_board(const int (*board)[BOARD_SIZE]);
+void set_hand(int* x, int* y);
 
 int main() {
 
   int board[BOARD_SIZE][BOARD_SIZE];
+
   init_board(board);
   show_board(board);
-
-  int turn = 0;
-  
-  while (turn++ < 5) {
+  int turn = 1;
+    
+  while (1) {
+    printf("turn = %d\n", turn);
+    turn += 1;
     int x, y;
-    do {
-      puts("Input the position !\n"
-           "[x y]");
-      scanf("%d %d", &x, &y);
-    } while (!is_available_position(x, y, board));
-    board[y][x] = (turn%2) ? BLACK : WHITE;
+    set_hand(&x, &y);
+    board[y-1][x-1] = (turn % 2) ? BLACK : WHITE;
     show_board(board);
   }
-  puts("See you ~~");
   
   return 0;
 }
@@ -43,28 +40,28 @@ char convert_stone_to_char(int stone) {
     ' ';
 }
 
-void init_board(int board[][BOARD_SIZE]) {
+void init_board(int (*board)[BOARD_SIZE]) {
   int i, j;
   for (i = 0; i < BOARD_SIZE; i++)
     for (j = 0; j < BOARD_SIZE; j++)
       board[i][j] = SPACE;
 }
 
-void show_board(const int board[][BOARD_SIZE]) {
+void show_board(const int (*board)[BOARD_SIZE]) {
   int i, j;
-  puts("----------------------------");
+  puts("-------------------------");
   printf("  ");
-  for (i = 0; i < BOARD_SIZE; putchar(' '), i++) printf("%d", i);
+  for (i = 0; i < BOARD_SIZE; i++) printf("%d ", i+1);
   putchar('\n');
-  for (i = 0; i < BOARD_SIZE; putchar('\n'), i++) { 
-    printf("%d ", i);
-    for (j = 0; j < BOARD_SIZE; putchar(' '), j++) 
+  for (i = 0; i < BOARD_SIZE; putchar('\n'), i++) {
+    printf("%d ", i+1);
+    for (j = 0; j < BOARD_SIZE; putchar(' '), j++)
       putchar(convert_stone_to_char(board[i][j]));
   }
-  putchar('\n');
-  puts("--------------------------");
+  puts("-------------------------");
 }
 
-int is_available_position(int x, int y, const int board[][BOARD_SIZE]) {
-  return (0 <= x && x < BOARD_SIZE) && (0 <= y && y < BOARD_SIZE) && (board[y][x] == SPACE);
+void set_hand(int* x, int* y) {
+  printf("Input your hand ! [x y]\n> ");
+  scanf("%d %d", x, y);
 }
