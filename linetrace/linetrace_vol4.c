f4b1078db991464193ad51d8e5e53c323e0f3c07
tanacchi <q111026d@mail.kyutech.jp>
2017-05-17 15:36:36 +0900

    Add draft of vol4

--- linetrace/linetrace_vol4.c
+++ linetrace/linetrace_vol4.c
@@ -2,3 +2,32 @@
 #include "gpio.h"
 #include "vs-wrc103.h"
 #include "ixbus.h"
+
+typedef struct  script_t {
+  unsigned short position;
+  int next_sign;
+  
+} Script;
+
+/*
+
+INIT
+
+READ
+
+UPDATE
+
+RUN
+
+*/
+
+
+/*
+
+INPUT : sesor
+
+JUDGE : WW, WB, BW, BB, W*, B*, *W, *B, **
+
+OUTPUT : motor
+
+*/
