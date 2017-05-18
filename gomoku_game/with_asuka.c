af616308ea0adf4bff3859a5681ed120448793b6
tanacchi <q111026d@mail.kyutech.jp>
2017-05-18 23:38:34 +0900

    Update gomoku game

--- gomoku_game/with_asuka.c
+++ gomoku_game/with_asuka.c
@@ -16,6 +16,9 @@ void show_board(const int (*board)[BOARD_SIZE]);
 void set_hand(int* x, int* y);
 int is_available_position(int x, int y, const int (*board)[BOARD_SIZE]);
 int count_space(const int (*board)[BOARD_SIZE]);
+int is_inside_board(int x, int y);
+int check_length(int x, int y, const int (*board)[BOARD_SIZE]);
+int is_game_finish(const int (*board)[BOARD_SIZE]);
 
 int main() {
   srand((unsigned)time(NULL));
@@ -33,9 +36,7 @@ int main() {
     } while (!is_available_position(x, y, board));
     board[y][x] = (turn%2) ? WHITE : BLACK;
     show_board(board);
-    
-    int i;
-    for (i = 0; i < 100000000; i++) ;
+    if (is_game_finish(board)) break;
   }
   return 0;
 }
@@ -78,7 +79,7 @@ void set_hand(int* x, int* y) {
 }
 
 int is_available_position(int x, int y, const int (*board)[BOARD_SIZE]) {
-  return (0 <= x && x < BOARD_SIZE) && (0 <= y && y < BOARD_SIZE) && (board[y][x] == SPACE);
+  return  is_inside_board(x, y) && (board[y][x] == SPACE);
 }
 
 int count_space(const int (*board)[BOARD_SIZE]) {
@@ -88,4 +89,33 @@ int count_space(const int (*board)[BOARD_SIZE]) {
     for (j = 0; j < BOARD_SIZE; j++)
       if (board[i][j] == SPACE) count++;
   return count;
+}
+
+int is_inside_board(int x, int y) {
+  return (0 <= x && x < BOARD_SIZE) && (0 <= y && y < BOARD_SIZE);
+}
+
+int check_length(int x, int y, const int (*board)[BOARD_SIZE]) {
+  const int dx[] = { 1, 1, 1, 0 };
+  const int dy[] = {-1, 0, 1, 1 };
+  int i, j;
+  int mystone = board[y][x];
+  for (i = 0; i < 4; i++) {
+    for (j = 1; is_inside_board(x+dx[i]*j, y+dy[i]*j); j++) {
+      if (mystone == board[y+dy[i]*j][x+dx[i]*j]) continue;
+      else if (j > 5) return 1;
+      else break;
+    }
+  }
+  return 0;
+}
+
+int is_game_finish(const int (*board)[BOARD_SIZE]) {
+  int i, j;
+  for (i = 0; i < BOARD_SIZE; i++) {
+    for (j = 0; j < BOARD_SIZE; j++) {
+      if (check_length(i, j, board)) return 1;
+    }
+  }
+  return 0;
 }
