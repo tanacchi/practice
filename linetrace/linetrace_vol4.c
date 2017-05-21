8ca7a0623acc0dc838f7f028ebce758dbe5f6665
tanacchi <q111026d@mail.kyutech.jp>
2017-05-21 14:42:44 +0900

    Update vol4

--- linetrace/linetrace_vol4.c
+++ linetrace/linetrace_vol4.c
@@ -3,103 +3,136 @@
 #include "vs-wrc103.h"
 #include "ixbus.h"
 
+#define MAIN_CYCLE 60
+
+enum Sign {
+  WW = 0x1 << 0,
+  WB = 0x1 << 1,
+  BW = 0x1 << 2,
+  BB = 0x1 << 3
+};
+
 typedef struct run_script_t {
-  int next_sign;
-  // func-pointer
-  const enum Sign {
-  }; 
+  int condition;
+  void (*run)(int);
 } Script;
 
 typedef struct machine_status_t {
-  int run_order;
+  int order;
   int position;
-  // func-pointer
+  const int* const threshold;
+  const Script* const script;
 } Status;
 
-/*
-
-  init (threshold, order->script)
-
-  write
-
-  check position
-
-  compare
-
-  set 
-
-  run
-
-*/
-
-/*
-
-  INPUT : sesor(avarage)
-
-  JUDGE : WW, WB, BW, BB, W*, B*, *W, *B, **
-
-  OUTPUT : motor
-
-*/
-
-/*
-
-WW : 0001 (0x1)
-
-WB : 0010 (0x2)
-
-BW : 0100 (0x4)
-
-BB : 1000 (0x8)
-
-WB|WW = 0011 (0x3)
-
-BW|WB|WW = 0111 (0x7) = ~BB
-
-*/
-
 void run_motor(short left, short right);
-/* const int get_threshold(); */
-int get_position();
-
-Status update(Status);
+const int* set_threshold();
+int get_position(const int* const threshold);
+void update(Status* status);
 
 void run_motor(short left, short right) {
   Mtr_Run_lv(right, -left, 0, 0, 0, 0);
 }
 
-/* const int get_threshold() { */
-/*   int buff[2]; */
-/*   int i, j; */
-/*   for (i = 0; i < 2; i++) { */
-/*     LED(i+1); */
-/*     while (!getSW()) ; */
-/*     for (j = 0; j < 10; j++) buff[i] += ADRead(i); */
-/*     while (getSW()) ; */
-/*   } */
-/*   return (buff[0]/10 + buff[1]/10) / 2; */
-/* } */
-
-/* int get_position() { */
-/*   return (ADReas(0)) */
-/* } */
-
-Status update(Status status) {
-  status.position = get_position(status.threshold);
-  if ((0x1 << status.position)&(status.script[status.order].condition)&000f) order++;
+const int* set_threshold() {
+  int buff[2][2] = { {0, 0}, {0, 0} };
+  for (int i = 0; i < 2; i++) {
+    LED(i+1);
+    while (!getSW()) ;
+    for (int j = 0; j < 20; j++) buff[i][j%2] += ADRead(j%2);
+    while (getSW()) ;
+    Wait(100);
+  }
+  int dist[2];
+  for (int i = 0; i < 2; i++) dist[i] = (buff[0][i] + buff[1][i]) / 2;
+  return dist;
 }
+
+void wait_start_switch() {
+  while (!getSW()) ;
+  while (!getSW()) ;
+  Wait(100);
+}
+
+int get_position(const int* threshold) {
+  return (((ADRead(0) > threshold[0]) << 1)|(ADRead(1) > threshold[1]));
+}
+
+void update(Status* status) {
+  status->position = get_position(status->threshold);
+  if (!((0x1 << status->position)&(status->script[status->order].condition))) status->order++;
+}
+
+// ==================== Running pattern =========================================
+
+void twin_trace(int position) {
+  static const short param[][2] = {
+    { 10000, 10000 },
+    { 10000, 0     },
+    { 0    , 10000 },
+    { 10000, 10000 }
+  };
+  run_motor(param[position][0], param[position][1]);
+}
+
+void twin_tight(int position) {
+  static const short param[][2] = {
+    { 10000, 10000 },
+    { 10000,-5000  },
+    { -5000, 10000 },
+    { 10000, 10000 }
+  };
+  run_motor(param[position][0], param[position][1]);
+}
+
+void no_motion(int position) {
+  run_motor(0, 0);
+}
+
+void go_straight(int position) {
+  run_motor(10000 , 10000);
+}
+
+void twist(int position) {
+ static const  short param[][2] = {
+    { 10000, 10000 },
+    { 0,    -10000 },
+    {-10000, 0     },
+    { 10000, 10000 }
+  };
+  run_motor(param[position][0], param[position][1]);
+}
+
+// =============================================================================
 
 int main(int argc, char** argv) {
 
-  const unsigned short main_circle = 60;
-  Init(main_circle);
+  Init(MAIN_CYCLE);
 
-  Script script;
-  Status status;
-  
+  const Script const script_list[] = {
+    {~BB, go_straight },
+    { BB, go_straight },
+    {~BB, twin_trace  },
+    {~WW, go_straight },
+    { WW, go_straight },
+    {~BB, twist       },
+    { BB, go_straight },
+    {~BB, twin_tight  },
+    {~WW, no_motion   }
+  };
+
+  Status status = {
+    0,
+    0x0,
+    set_threshold(),
+    script_list
+  };
+
+  wait_start_switch();
   while (1) {
-    status = update(stauts);
-    status.run();
-    // status.script[status.order].run(status.position);
+    LED(status.order%4);
+    update(&status);
+    status.script_list[status.order].run(status.position);
   }
+
+  return 0;
 }
