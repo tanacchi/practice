042a4c531b14c5f4aeca99a3d95f3601ec341f51
tanacchi <q111026d@mail.kyutech.jp>
2017-08-30 09:55:49 +0900

    Add MineSweeper class

---
+++ minesweeper/minesweeper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class MineSweeper {
+public:
+  MineSweeper();
+  MineSweeper(const MineSweeper& src);
+  const MineSweeper& operator=(const MineSweeper& src);
+  ~MineSweeper();
+private:
+};
+
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
