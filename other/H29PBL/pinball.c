186e2d802c7e54a2d1e240cbc09be9c071f31918
tanacchi <q111026d@mail.kyutech.jp>
2017-12-08 00:22:46 +0900

    Update rand make

--- other/H29PBL/pinball.c
+++ other/H29PBL/pinball.c
@@ -2,25 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-enum { Left = 0, Right = 1 };
+enum { Left = -1, Right = 1 };
 
-float initThreshold(float left_ratio, float right_ratio);
-int getDirectoin(float threshold);
+typedef double DataType;
+
+DataType genThreshold(const float left_ratio, const float right_ratio)
+{
+  return left_ratio / (left_ratio + right_ratio);
+}
+
+DataType getRand()
+{
+  return (DataType)rand() / ((DataType)RAND_MAX + 1.0);
+}
+
+int getDirectoin(const float threshold)
+{
+  return (getRand() < threshold) ? Left : Right;
+}
 
 int main()
 {
   srand((unsigned)time(NULL));
+  const DataType threshold = genThreshold(6.0l, 4.0l);
 
+  float count = 0;
+  const float times = 100000;
+  for (int i = 0; i < times; ++i) {
+    if (getDirectoin(threshold) == -1) count++;
+  }
+  
+  printf("%.10f\n", count / times);
   return 0;
 }
-
-float initThreshold(float left_ratio, float right_ratio)
-{
-  return left_ratio / (left_ratio + right_ratio) * 100.0;
-}
-
-int getDirectoin(float threshold)
-{
-  return (threshold < rand() % 100) ? Left : Right;
-}
-
