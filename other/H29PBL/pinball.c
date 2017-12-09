416c9ff5b4a2fe6da05baab3eb762a00d0b3f10b
tanacchi <q111026d@mail.kyutech.jp>
2017-12-09 18:56:20 +0900

    Update pinball.c

--- other/H29PBL/pinball.c
+++ other/H29PBL/pinball.c
@@ -2,36 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 
-enum { Left = -1, Right = 1 };
+#define _widthof(array) (int)(sizeof(array) / sizeof(*array))
 
-typedef double DataType;
+static const double left_score  = -1.0;
+static const double right_score =  1.0;
 
-DataType genThreshold(const float left_ratio, const float right_ratio)
+double initThreshold(const double left_ratio, const double right_ratio)
 {
   return left_ratio / (left_ratio + right_ratio);
 }
 
-DataType getRand()
+double genRand()
 {
-  return (DataType)rand() / ((DataType)RAND_MAX + 1.0);
+  return (double)rand() / ((double)RAND_MAX + 1.0);
 }
 
-int getDirectoin(const float threshold)
+double getScore(const double threshold)
 {
-  return (getRand() < threshold) ? Left : Right;
+  return (genRand() < threshold) ? left_score : right_score;
+}
+
+int getAccessNum(const int position, const int* array)
+{
+  return position + _widthof(array) / 2;
+}
+
+const int* getSimResult(const double threshold)
+{
+  const int ball_num = 100000, max_depth = 501;
+  const double score_unit = 0.5;
+
+  static int position_list[max_depth];
+  for (int i = 0; i < ball_num; ++i) {
+    double position = 0;
+    for (int depth = 1; depth < max_depth; ++depth)
+      position += score_unit * (double)getScore(threshold);
+    position_list[(int)position + (int)(max_depth/2)]++;
+  }
+  return position_list;
 }
 
 int main()
 {
   srand((unsigned)time(NULL));
-  const DataType threshold = genThreshold(6.0l, 4.0l);
-
-  float count = 0;
-  const float times = 100000;
-  for (int i = 0; i < times; ++i) {
-    if (getDirectoin(threshold) == -1) count++;
-  }
+  const double threshold = initThreshold(1.0l, 1.0l);
   
-  printf("%.10f\n", count / times);
+  const int* result = getSimResult(threshold);
+  for (int i = 0; i < 501; ++i)
+    printf("%d\n", result[i]);
   return 0;
 }
