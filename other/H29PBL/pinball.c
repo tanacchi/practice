457d38ce5886e2b6645fb38a63382d785c0c6ff7
tanacchi <q111026d@mail.kyutech.jp>
2017-12-11 19:24:26 +0900

    Finish pinball

--- other/H29PBL/pinball.c
+++ other/H29PBL/pinball.c
@@ -22,40 +22,45 @@ double getScore(const double threshold)
   return (genRand() < threshold) ? left_score : right_score;
 }
 
-int getAccessNum(const int position, const int* array)
-{
-  return position + _widthof(array) / 2;
-}
-
-void simPinball(const double threshold, const char* const file_path)
+int simPinball(const double threshold, const char* const file_path)
 {
   const int ball_num = 1000, max_depth = 501;
   const double score_unit = 0.5;
-  static int position_list[max_depth];
-  
+  int position_list[max_depth];
+  for (int i = 0; i < _widthof(position_list); position_list[i++] = 0);
   for (int i = 0; i < ball_num; ++i) {
     double position = 0;
-    for (int depth = 1; depth < max_depth; ++depth)
-      position += score_unit * (double)getScore(threshold);
+    for (int depth = 1; depth < max_depth; ++depth) position += score_unit * (double)getScore(threshold);
     position_list[(int)position + (int)(max_depth/2)]++;
   }
   
   FILE* filep = fopen(file_path, "w");
   for (int i = 0; i < _widthof(position_list); ++i) fprintf(filep, "%d\t%d\n", (int)(i-_widthof(position_list)/2), position_list[i]);
   fclose(filep);
+
+  return position_list[_widthof(position_list)/2];
 }
 
 int main()
 {
   srand((unsigned)time(NULL));
-  const double threshold = initThreshold(1.0l, 1.0l);
-
-  const char* const data1_file_path = "data1.dat";
-  simPinball(threshold, data1_file_path);
-
-  FILE *gplotp = popen("gnuplot -persist","w");
-  fprintf(gplotp, "plot \"%s\" with lines linetype 1 title \"data1\"\n", data1_file_path);
-  pclose(gplotp);
-  
+  {
+    const double threshold = initThreshold(1.0l, 1.0l);
+    const char* const file_path = "data.dat";
+    int center_balls = simPinball(threshold, file_path);
+    printf("x = 0, y = %d\n", center_balls);
+    FILE *gplotp = popen("gnuplot -persist", "w");
+    fprintf(gplotp, "plot \"%s\" w l lw 3 linetype 1 title \"1\"\n", file_path);
+    pclose(gplotp);
+  }
+  {
+    const double threshold = initThreshold(6.0l, 4.0l);
+    const char* const file_path = "data.dat";
+    int center_balls = simPinball(threshold, file_path);
+    printf("x = 0, y = %d\n", center_balls);
+    FILE *gplotp = popen("gnuplot -persist", "w");
+    fprintf(gplotp, "plot \"%s\" w l lw 3 linetype 1 title \"2\"\n", file_path);
+    pclose(gplotp);
+  }
   return 0;
 }
