610ed33bb761e19982a4eb1853a8ede6920cefb6
tanacchi <q111026d@mail.kyutech.jp>
2017-12-12 17:17:16 +0900

    Update main code
    コンストラクタがちゃんと動いているのがちょっと不思議

--- other/H29PBL/BiotSavart.cpp
+++ other/H29PBL/BiotSavart.cpp
@@ -1,39 +1,67 @@
+#include <array>
 #include <iostream>
 #include <cmath>
 
 using DataType = double;
 
-namespace {
-
+namespace Vector {
+  enum { x = 0, y = 1, z = 2 };
   class Vector {
   public:
     Vector(DataType x = 0.0, DataType y = 0.0, DataType z = 0.0)
-      : elem_{x , y, z}
+      : elem_{x, y, z}
     {
     }
-    const Vector& operator-=(const Vector& rhs) const
+    const DataType& operator[](int n) const
     {
+      return elem_[n];
+    }
+    const Vector& operator+=(const Vector rhs)
+    {
+      for (auto i{0}; i < elem_.size(); ++i) elem_[i] += rhs.elem_[i];
+      return *this;
+    }
+    const Vector& operator-=(const Vector rhs)
+    {
+      for (auto i{0}; i < elem_.size(); ++i) elem_[i] -= rhs.elem_[i];
+      return *this;
+    }
+    void show()
+    {
+      std::cout << "========================" << std::endl;
+      std::cout << "x = " << elem_[x] << '\n'
+                << "y = " << elem_[y] << '\n'
+                << "z = " << elem_[z] << '\n' << std::flush;
+      std::cout << "========================" << std::endl;
     }
   private:
-    enum { x = 0, y = 1, z = 2; };
-    DataType elem_[3];
+    std::array<DataType, 3> elem_;
   };
-
-  
-}
-
-Vector getCross(Vector a, Vector b)
-{
-  Vector ans = {
-    a.y*b.z - a.z*b.y,
-    a.z*b.x - a.x*b.z,
-    a.x*b.y - a*y*b.x;
-  };
-  return ans;
-}
+  Vector operator+(const Vector A, const Vector B)
+  {
+    return {A[x] + B[x],  A[y] + B[y], A[z] + B[z]};
+  }
+  Vector operator-(const Vector A, const Vector B)
+  {
+    return {A[x] - B[x],  A[y] - B[y], A[z] - B[z]};
+  }
+  Vector operator*(const Vector A, const Vector B)
+  {
+    return {A[y]*B[z] - A[z]*B[y], A[z]*B[x] - A[x]*B[z], A[x]*B[y] - A[y]*B[x]};
+  }
+};
 
 int main ()
 {
-
+  {
+    Vector::Vector v1{3.3, 2.1, 5.9};
+    v1 -= Vector::Vector{1.1, 1.1, 1.1};
+    v1.show();
+  }
+  {
+    Vector::Vector v2{1.0, 0.0, 0.0};
+    Vector::Vector v3{0.0, 1.0, 0.0};
+    (v2*v3).show();
+  }
   return 0;
 }
