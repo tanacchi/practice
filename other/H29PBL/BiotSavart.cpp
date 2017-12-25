5d4bb2d6f514f2af56f887cf4c9183d997ead087
tanacchi <q111026d@mail.kyutech.jp>
2017-12-25 16:07:36 +0900

    Modify for 3d plot

--- other/H29PBL/BiotSavart.cpp
+++ other/H29PBL/BiotSavart.cpp
@@ -172,15 +172,16 @@ Vector::Vector getMagneticVector(const Vector::Vector& r, std::initializer_list<
 int main ()
 {
   { // Mission 1
-    const Vector::Vector r{0.0, 0.0, 0.0};
-    Route route1{[](DataType x){ return std::sqrt(1 - x*x); }, {-1.0, 1.0}};
-    Route route2{[](DataType x){ return -std::sqrt(1 - x*x); }, {1.0, -1.0}};
-    ElectricCurrent I1{route1, 1.0}; ElectricCurrent I2{route2, 1.0};
-    Vector::Vector B;
-    B += getMagneticVector(r, {I1, I2});    
+    const Route route1{[](DataType x){ return std::sqrt(1 - x*x); }, {-1.0, 1.0}};
+    const Route route2{[](DataType x){ return -std::sqrt(1 - x*x); }, {1.0, -1.0}};
+    const ElectricCurrent I1{route1, 1.0}; const ElectricCurrent I2{route2, 1.0};
     std::fstream fstream{"mission1.dat", std::ios_base::out | std::ios_base::trunc};
-    B.show(fstream);
-    B.show(std::cout);
+    for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.075)
+      for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.075) {
+        Vector::Vector B{getMagneticVector(r, {I1, I2})};
+        fstream << r[index::x] << '\t' << r[index::y] << '\t' << B[index::z] << std::endl;
+      }
+    fstream << std::endl;
   }
   { // Mission 2
     const Vector::Vector r{0.0, 0.0, 0.0};
