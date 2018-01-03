5096d47ba5a1f2159d0bc9d76fc79cd24642a77a
tanacchi <q111026d@mail.kyutech.jp>
2018-01-03 21:48:32 +0900

    Remake sample of map

--- cpp_edision/stl_sample/map.cpp
+++ cpp_edision/stl_sample/map.cpp
@@ -3,11 +3,14 @@
 
 int main()
 {
-  std::map<char, int> map{};
-  for (int i = 0; i < 'z'-'a'+1; ++i)
-    map.insert(std::pair<char, int>{'a'+i, i+1});
-
-  for (int i = 0; i < 'z'-'a'+1; ++i)
-    std::cout << static_cast<char>('z'-i)<< ' ' << map.at('z'-i) << std::endl;
+  const std::map<const unsigned short, const std::string> month_map = {
+    {1, "January"},  {2, "February"},  {3, "March"},
+    {4, "April"},    {5, "May"},       {6, "June"},
+    {7, "July"},     {8, "August"},    {9, "September"},
+    {10, "October"}, {11, "November"}, {12, "December"}
+  };
+  int input_month{};
+  std::cin >> input_month;
+  std::cout << month_map.at(input_month) << std::endl;
   return 0;
 }
