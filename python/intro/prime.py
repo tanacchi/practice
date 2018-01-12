ef28de73fa07c3a586b025d7b0d57257e7905ec9
tanacchi <q111026d@mail.kyutech.jp>
2018-01-12 13:38:58 +0900

    Finish prime.py

---
+++ python/intro/prime.py
@@ -0,0 +1,22 @@
+def isPrime(num):
+    limit = int(num)
+    for i in range(2, limit):
+        if ((limit % i) == 0): return False
+    return True
+
+def getListOfPrime(limit):
+    prime_list = []
+    for i in range(2, limit):
+        if (isPrime(i)): prime_list.append(i)
+    return prime_list
+    
+num = input('Plaese put a number\n>')
+if (not num.isdigit()):
+    print('Please put a number!!')
+    exit()
+else:
+    limit = int(num)
+
+
+prime_list = getListOfPrime(limit)
+print(prime_list)
