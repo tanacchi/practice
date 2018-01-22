1e692cde8302e9ae975ecb37157bc22973aff9f1
tanacchi <q111026d@mail.kyutech.jp>
2018-01-22 18:43:54 +0900

    Add mouse input

--- python/game/maru_batsu.py
+++ python/game/maru_batsu.py
@@ -3,19 +3,33 @@ import pygame
 from pygame.locals import *
 import sys
 
-def main():
+SPACE = 0
+MARU  = 1
+BATSU = 2
+
+pos_list = (
+    (100, 100), (300, 100), (500, 100),
+    (100, 300), (300, 300), (500, 300),
+    (100, 500), (300, 500), (500, 500)
+)
+
+def main():    
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("maru_batsu")
 
-    #    mark_board = [0 for i in range(9)]
-    mark_board = [0, 1, 2, 0, 1, 2, 0, 1, 2]
+    mark_board = [SPACE for i in range(9)]
+    turn = MARU
+    
     while (True):
         pygame.display.update()
         pygame.time.wait(30)
         screen.fill((200, 200, 200, 50));
         show(screen, mark_board)
-        
+
+        if update_board(mark_board, turn):
+            if turn == MARU: turn = BATSU
+            else: turn = MARU
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -25,17 +39,29 @@ def main():
                     pygame.quit()
                     sys.exit()
 
+
+def update_board(board_object, turn):
+    pressed_mouse = pygame.mouse.get_pressed()
+    if pressed_mouse[0]:
+        x, y = pygame.mouse.get_pos()
+        for i in range(9):
+            if is_inside(x, y, pos_list[i][0], pos_list[i][1]) and board_object[i] == SPACE:
+                board_object[i] = turn
+                return True
+    return False
+
+def is_inside(x, y, range_x, range_y):
+    if range_x-100 < x and x < range_x+100 and range_y-100 < y and y < range_y+100:
+        return True
+    else:
+        return False
+    
 def show(display_object, board_object):
     draw_field(display_object)
-    pos_list = ( # REFACT NEEDED
-        (100, 100), (300, 100), (500, 100),
-        (100, 300), (300, 300), (500, 300),
-        (100, 500), (300, 500), (500, 500)
-    )
     for i in range(9):
-           target = board_object[i]
-           if target == 1: draw_maru(display_object, pos_list[i][0], pos_list[i][1])
-           if target == 2: draw_batsu(display_object, pos_list[i][0], pos_list[i][1])
+        target = board_object[i]
+        if target == MARU: draw_maru(display_object, pos_list[i][0], pos_list[i][1])
+        if target == BATSU: draw_batsu(display_object, pos_list[i][0], pos_list[i][1])
     
 def draw_field(display_object):
     pygame.draw.line(display_object, (0,200,0), (200,0), (200, 600), 5)
@@ -44,11 +70,11 @@ def draw_field(display_object):
     pygame.draw.line(display_object, (0,200,0), (0,400), (600, 400), 5)
 
 def draw_maru(display_object, x, y):
-    pygame.draw.circle(display_object, (200, 0, 0), (x, y), 50, 5)
+    pygame.draw.circle(display_object, (200, 0, 0), (x, y), 50, 10)
 
 def draw_batsu(display_object, x, y):
-    pygame.draw.line(display_object, (0, 0, 200), (x-50, y-50), (x+50, y+50), 5)
-    pygame.draw.line(display_object, (0, 0, 200), (x-50, y+50), (x+50, y-50), 5)
+    pygame.draw.line(display_object, (0, 0, 200), (x-50, y-50), (x+50, y+50), 10)
+    pygame.draw.line(display_object, (0, 0, 200), (x-50, y+50), (x+50, y-50), 10)
     
 if __name__=="__main__":
     main()
