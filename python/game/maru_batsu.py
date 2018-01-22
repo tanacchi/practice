0270a8e0a1bcf070c34dbab821d739700ec37f68
tanacchi <q111026d@mail.kyutech.jp>
2018-01-22 15:58:31 +0900

    Finish show function

--- python/game/maru_batsu.py
+++ python/game/maru_batsu.py
@@ -8,17 +8,15 @@ def main():
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("maru_batsu")
 
-    mark_board = [0 for i in range(9)]
-    print(mark_board)
-
+    #    mark_board = [0 for i in range(9)]
+    mark_board = [0, 1, 2, 0, 1, 2, 0, 1, 2]
     while (True):
         pygame.display.update()
         pygame.time.wait(30)
         screen.fill((200, 200, 200, 50));
-        draw_field(screen)
+        show(screen, mark_board)
         
         for event in pygame.event.get():
-            print(type(event))
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -27,14 +25,30 @@ def main():
                     pygame.quit()
                     sys.exit()
 
-def draw_board(display_object, board_object):
+def show(display_object, board_object):
+    draw_field(display_object)
+    pos_list = ( # REFACT NEEDED
+        (100, 100), (300, 100), (500, 100),
+        (100, 300), (300, 300), (500, 300),
+        (100, 500), (300, 500), (500, 500)
+    )
+    for i in range(9):
+           target = board_object[i]
+           if target == 1: draw_maru(display_object, pos_list[i][0], pos_list[i][1])
+           if target == 2: draw_batsu(display_object, pos_list[i][0], pos_list[i][1])
     
-                    
 def draw_field(display_object):
     pygame.draw.line(display_object, (0,200,0), (200,0), (200, 600), 5)
     pygame.draw.line(display_object, (0,200,0), (400,0), (400, 600), 5)
     pygame.draw.line(display_object, (0,200,0), (0,200), (600, 200), 5)
     pygame.draw.line(display_object, (0,200,0), (0,400), (600, 400), 5)
+
+def draw_maru(display_object, x, y):
+    pygame.draw.circle(display_object, (200, 0, 0), (x, y), 50, 5)
+
+def draw_batsu(display_object, x, y):
+    pygame.draw.line(display_object, (0, 0, 200), (x-50, y-50), (x+50, y+50), 5)
+    pygame.draw.line(display_object, (0, 0, 200), (x-50, y+50), (x+50, y-50), 5)
     
 if __name__=="__main__":
     main()
