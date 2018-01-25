bdfc67c6dafe8b4eb2cdee84c356d2b560385b07
tanacchi <q111026d@mail.kyutech.jp>
2018-01-25 16:12:28 +0900

    Add isnert method

--- python/game/othello.py
+++ python/game/othello.py
@@ -3,22 +3,29 @@ import pygame
 from pygame.locals import *
 import sys
 
+SPACE =  0
+WHITE =  1
+BLACK =  2
+
 class Board:
-    SPACE = 0
-    WHITE = 1
-    BLACK = 2
     def __init__(self, width = 8, height = 8):
         self.__width = width
         self.__height = height
-        self.__board = [[self.SPACE for x in range(width)] for y in range(height)]
+        self.__board = [[SPACE for x in range(width)] for y in range(height)]
+        self.__active_stone = WHITE
 
     def show(self):
-        print(self.__width)
-        print(self. __height)
+        print("==========================")
         for row in self.__board: print(row)
-        
+        print("==========================")
+
+    def insert(self, x, y, stone):
+        self.__board[y][x] = stone
+
 def main():
     board = Board(8, 8)
+    board.show()
+    board.insert(2, 2, BLACK)
     board.show()
 
 if __name__ == "__main__":
