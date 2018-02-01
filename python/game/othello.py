52139ff2139dbc46f8381a8c155aeba6c14f2e75
tanacchi <q111026d@mail.kyutech.jp>
2018-02-01 18:58:18 +0900

    Update show method

--- python/game/othello.py
+++ python/game/othello.py
@@ -31,32 +31,58 @@ class Board:
         return tuple(self.__board)
 
 class Display:
-    def __init__(self, width: int, height: int, row = 8: int, col = 8: int):
+    def __init__(self, width: int, height: int, row = 8, col = 8):
         self.__width = width
         self.__height = height
         self.__row = row
         self.__col = col
+        row_unit = height // row
+        col_unit = width  // col
         self.__screen = pygame.display.set_mode((width, height))
-        self.__position_list = [[]]
+        calc_pos = lambda i, unit: i*unit + unit//2
+        self.__position_list = []
+        for r in range(row):
+            for c in range(col):
+                self.__position_list.append([calc_pos(c, col_unit), calc_pos(r, row_unit)])
         pygame.display.set_caption("othello")
 
+    def draw_partition(self):
+        row_unit = self.__height // self.__row
+        col_unit = self.__width  // self.__col
+        for r in range(self.__row):
+            pygame.draw.line(self.__screen, (150,150,150), (0,r*row_unit), (self.__width, r*row_unit), 5)
+        for c in range(self.__row):
+            pygame.draw.line(self.__screen, (150,150,150), (c*col_unit,0), (c*col_unit, self.__height), 5)
+        
+    def draw_stone(self, index, stone):
+        if stone == SPACE: pass
+        elif stone == WHITE:
+            pygame.draw.circle(self.__screen, (255,255,255), self.__position_list[index], 30)
+        elif stone == BLACK:
+            pygame.draw.circle(self.__screen, (0,0,0), self.__position_list[index], 30)
+        else: raise "Invalid value of stone."
+        
     def show(self, board: Board):
         pygame.time.wait(30)
         self.__screen.fill((0, 150, 0))
         board_data = board.get_data()
-        
+        self.draw_partition()
+        for i in range(len(board_data)):
+            self.draw_stone(i, board_data[i])
         pygame.display.update()
 
-def main():
-    pygame.init()
-    board = Board(8, 8)
-    board.cout()
-    board.insert(2, 5, BLACK)
-    board.cout()
-    display = Display(600, 600, board)
-    
-    for _ in range(100):
-        display.show(board)
-
+class GameMaster:
+    def __init__(self, width, height, row, col):
+        pygame.init()
+        self.__display = Display(width, height, row, col)
+        self.__board   = Board(row, col)
+    def run(self):
+        self.__board.cout()
+        self.__board.insert(2, 5, BLACK)
+        self.__board.cout()
+        for _ in range(100):
+            self.__display.show(self.__board)
+        
 if __name__ == "__main__":
-    main()
+    master = GameMaster(800, 800, 8, 8)
+    master.run()
