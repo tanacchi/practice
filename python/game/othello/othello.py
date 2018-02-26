94c462136c12599886fdea428d418610fb49faf0
tanacchi <q111026d@mail.kyutech.jp>
2018-02-26 18:52:03 +0900

    Modify for board module

--- python/game/othello/othello.py
+++ python/game/othello/othello.py
@@ -5,42 +5,29 @@ from enum import Enum
 import board
 
 class Stone(Enum):
-    SPACE = ' '
-    WHITE = 'O'
-    BLACK = 'X'
-
+    SPACE = lambda screen, pos: pygame.draw.circle(screen, (0,150,0), pos, 30)
+    WHITE = lambda screen, pos: pygame.draw.circle(screen, (255,255,255), pos, 30)
+    BLACK = lambda screen, pos: pygame.draw.circle(screen, (0,0,0), pos, 30)
     
 class Display:
-    def __init__(self, width: int, height: int, row = 8, col = 8):
-        self.__width = width
-        self.__height = height
-        self.__row = row
-        self.__col = col
-        row_unit = height // row
-        col_unit = width  // col
-        self.__screen = pygame.display.set_mode((width, height))
+    def __init__(self, board_info:board.BoardInfo):
+        self.info = board_info
+        self.__screen = pygame.display.set_mode((self.info.width, self.info.height))
         calc_pos = lambda i, unit: i*unit + unit//2
         self.__position_list = []
-        for r in range(row):
-            for c in range(col):
-                self.__position_list.append([calc_pos(c, col_unit), calc_pos(r, row_unit)])
+        for r in range(self.info.row):
+            for c in range(self.info.col):
+                self.__position_list.append([calc_pos(c, self.info.col_unit), calc_pos(r, self.info.row_unit)])
         pygame.display.set_caption("othello")
 
     def draw_partition(self):
-        row_unit = self.__height // self.__row
-        col_unit = self.__width  // self.__col
-        for r in range(self.__row):
-            pygame.draw.line(self.__screen, (150,150,150), (0,r*row_unit), (self.__width, r*row_unit), 5)
-        for c in range(self.__row):
-            pygame.draw.line(self.__screen, (150,150,150), (c*col_unit,0), (c*col_unit, self.__height), 5)
+        for r in range(self.info.row):
+            pygame.draw.line(self.__screen, (150,150,150), (0,r*self.info.row_unit), (self.info.width, r*self.info.row_unit), 5)
+        for c in range(self.info.col):
+            pygame.draw.line(self.__screen, (150,150,150), (c*self.info.col_unit,0), (c*self.info.col_unit, self.info.height), 5)
         
     def draw_stone(self, index, stone):
-        if stone == Stone.SPACE: pass
-        elif stone == Stone.WHITE:
-            pygame.draw.circle(self.__screen, (255,255,255), self.__position_list[index], 30)
-        elif stone == Stone.BLACK:
-            pygame.draw.circle(self.__screen, (0,0,0), self.__position_list[index], 30)
-        else: raise "Invalid value of stone."
+        stone(self.__screen, self.__position_list[index])
         
     def show(self, board: board.Board):
         pygame.time.wait(30)
@@ -56,10 +43,10 @@ class Display:
             pass
         
 class GameMaster:
-    def __init__(self, width, height, row, col):
+    def __init__(self, board_info):
         pygame.init()
-        self.__display = Display(width, height, row, col)
-        self.__board   = board.Board(board.BoardInfo(8, 8, 800, 800), Stone.SPACE)
+        self.__display = Display(board_info)
+        self.__board   = board.Board((board_info), Stone.SPACE)
     def run(self):
         print(self.__board)
         self.__board.insert(2, 5, Stone.BLACK)
@@ -68,5 +55,5 @@ class GameMaster:
             self.__display.show(self.__board)
 
 if __name__ == "__main__":
-    master = GameMaster(800, 800, 8, 8)
+    master = GameMaster(board.BoardInfo(8, 9, 800, 800))
     master.run()
