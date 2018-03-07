d5375d88760dcba2aab05995fd634ad8b6ecea5f
tanacchi <q111026d@mail.kyutech.jp>
2018-03-07 15:48:51 +0900

    Split file with display class & game_master class

---
+++ python/game/othello/display.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+import board
+import othello_board
+
+class Display(object):
+    def __init__(self, board_info:board.BoardInfo):
+        self.info = board_info
+        self.__screen = pygame.display.set_mode((self.info.width, self.info.height))
+        calc_pos = lambda i, unit: i*unit + unit//2
+        self.__position_list = []
+        for r in range(self.info.row):
+            for c in range(self.info.col):
+                self.__position_list.append([calc_pos(c, self.info.col_unit), calc_pos(r, self.info.row_unit)])
+        pygame.display.set_caption("othello")
+
+    def draw_partition(self):
+        for r in range(self.info.row):
+            pygame.draw.line(self.__screen, (150,150,150), (0,r*self.info.row_unit), (self.info.width, r*self.info.row_unit), 5)
+        for c in range(self.info.col):
+            pygame.draw.line(self.__screen, (150,150,150), (c*self.info.col_unit,0), (c*self.info.col_unit, self.info.height), 5)
+        
+    def draw_stone(self, index, stone):
+        func = othello_board.Stone.funcs.get(stone)
+        func(self.__screen, self.__position_list[index])
+        
+    def show(self, board: board.Board):
+        pygame.time.wait(30)
+        self.__screen.fill((0, 150, 0))
+        board_data = board.get_data()
+        self.draw_partition()
+        for i, data in enumerate(board_data):
+            self.draw_stone(i, data)
+        pygame.display.update()
+
+    def get_position_number(self, x, y):
+        for i, position in enumerate(self.__position_list):
+            pass
+
+if __name__ == '__main__':
+    info = board.BoardInfo(8, 8, 800, 800)
+    board = othello_board.OthelloBoard(info)
+    disp = Display(info)
+
+    for i in range(100):
+        disp.show(board)
--- python/game/othello/game_master.py
+++ python/game/othello/game_master.py
@@ -1,46 +1,8 @@
-import pygame
-from pygame.locals import *
+#!/usr/bin/env python
+
 import sys
-from enum import Enum 
-import board
-
-class Stone(Enum):
-    SPACE = lambda screen, pos: pygame.draw.circle(screen, (0,150,0), pos, 30)
-    WHITE = lambda screen, pos: pygame.draw.circle(screen, (255,255,255), pos, 30)
-    BLACK = lambda screen, pos: pygame.draw.circle(screen, (0,0,0), pos, 30)
-    
-class Display(object):
-    def __init__(self, board_info:board.BoardInfo):
-        self.info = board_info
-        self.__screen = pygame.display.set_mode((self.info.width, self.info.height))
-        calc_pos = lambda i, unit: i*unit + unit//2
-        self.__position_list = []
-        for r in range(self.info.row):
-            for c in range(self.info.col):
-                self.__position_list.append([calc_pos(c, self.info.col_unit), calc_pos(r, self.info.row_unit)])
-        pygame.display.set_caption("othello")
-
-    def draw_partition(self):
-        for r in range(self.info.row):
-            pygame.draw.line(self.__screen, (150,150,150), (0,r*self.info.row_unit), (self.info.width, r*self.info.row_unit), 5)
-        for c in range(self.info.col):
-            pygame.draw.line(self.__screen, (150,150,150), (c*self.info.col_unit,0), (c*self.info.col_unit, self.info.height), 5)
-        
-    def draw_stone(self, index, stone):
-        stone(self.__screen, self.__position_list[index])
-        
-    def show(self, board: board.Board):
-        pygame.time.wait(30)
-        self.__screen.fill((0, 150, 0))
-        board_data = board.get_data()
-        self.draw_partition()
-        for i, data in enumerate(board_data):
-            self.draw_stone(i, data)
-        pygame.display.update()
-
-    def get_position_number(self, x, y):
-        for i, position in enumerate(self.__position_list):
-            pass
+import othello_board
+import display
         
 class GameMaster(object):
     def __init__(self, board_info):
