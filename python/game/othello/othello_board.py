f25bb46f6b938d962f4f35c46d214f2f4a63eda1
tanacchi <q111026d@mail.kyutech.jp>
2018-03-12 17:22:33 +0900

    Add 'get_reversible_length' method

--- python/game/othello/othello_board.py
+++ python/game/othello/othello_board.py
@@ -19,17 +19,33 @@ object_printer = \
 from board import BoardInfo, BoardBase
 
 class OthelloBoard(BoardBase):
+    dr_tuple = ((0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1))
+    
     def __init__(self, info):
         super(OthelloBoard, self).__init__(info, Stone.SPACE)
-        self.data[self.get_access_num(info.row//2-1, info.col//2-1)] \
-        = self.data[self.get_access_num(info.row//2, info.col//2)]   \
-        = Stone.WHITE
-        self.data[self.get_access_num(info.row//2, info.col//2-1)]   \
-        = self.data[self.get_access_num(info.row//2-1, info.col//2)]\
-        = Stone.BLACK
-
+        self.insert(info.row//2-1, info.col//2-1, Stone.WHITE)
+        self.insert(info.row//2, info.col//2, Stone.WHITE)
+        self.insert(info.row//2, info.col//2-1, Stone.BLACK)
+        self.insert(info.row//2-1, info.col//2, Stone.BLACK)
+        self.active_stone, self.enemy_stone = Stone.WHITE, Stone.BLACK
         
+    def switch_stone(self):
+        self.active_stone, self.enemy_stone = self.enemy_stone, self.active_stone
+
+    def get_reversible_length(self, x, y, dr):
+        length = 1
+        while self.is_inside(x + length*dr[0], y + length*dr[1]):
+            if self.data[self.get_access_num(x + length*dr[0], y + length*dr[1])] == self.active_stone:
+                return length - 1
+            elif self.data[self.get_access_num(x + length*dr[0], y + length*dr[1])] == Stone.SPACE:
+                break
+            length += 1
+        return 0
+
+    
 if __name__ == '__main__':
     info = BoardInfo(8, 8, 800, 800)
     board = OthelloBoard(info)
     print(board)
+    for dr in OthelloBoard.dr_tuple:
+        print(board.get_reversible_length(4, 2, dr))
