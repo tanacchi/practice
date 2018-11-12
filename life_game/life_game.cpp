cd4ed11e15471f67bfac41de2e1d297369602391
tanacchi <q111026d@mail.kyutech.jp>
2018-11-12 17:45:41 +0900

    Update life game
    バグ有りだが必要な動作は揃った。前回の状況を見て次の状態を決定しなければならないので
    単純な配列操作だけでは足りないみたい

--- life_game/life_game.cpp
+++ life_game/life_game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 #include <random>
+#include <chrono>
+#include <thread>
 
 class Cell
 {
@@ -15,6 +17,11 @@ public:
     return is_alive_ ? 'O' : ' ';
   }
 
+  bool is_alive() const
+  {
+    return is_alive_;
+  }
+  
   void revive()
   {
     is_alive_ = true;
@@ -48,6 +55,7 @@ public:
 
   void show() const
   {
+    std::cout << "===============================" << std::endl;
     for (auto row : field_)
     {
       for (auto elem : row)
@@ -56,18 +64,74 @@ public:
       }
       std::cout << std::endl;
     }
+    std::cout << "===============================" << std::endl;
+  }
+
+  bool in_range(int x, int y) const
+  {
+    return (0 <= x && x < field_[0].size()) && (0 <= y && y <= field_.size());
+  }
+  
+  void toggle_cell(std::size_t x, std::size_t y)
+  {
+    static const std::array<const std::pair<int, int>, 8ul > dirs{
+      {{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}}
+    };
+
+    std::size_t arrive_neighbor_count{0};
+    for (const auto& dir : dirs)
+    {
+      if (!in_range(x + dir.first, y + dir.second))
+      {
+        continue;
+      }
+      if (field_[y + dir.second][x + dir.first].is_alive())
+      {
+        ++arrive_neighbor_count;
+      }
+    }
+
+    Cell& target{field_[y][x]};
+    if (target.is_alive())
+    {
+      if (arrive_neighbor_count <= 1 || 4 <= arrive_neighbor_count)
+      {
+        target.kill();
+      }
+    } else {
+      if (arrive_neighbor_count == 3)
+      {
+        target.revive();
+      }
+    }
+  }
+
+  void update()
+  {
+    for (std::size_t y{0}; y < field_.size(); ++y)
+    {
+      for (std::size_t x{0}; x < field_[y].size(); ++x)
+      {
+        toggle_cell(x, y);
+      }
+    }
   }
 private:
   std::random_device seed_gen_;
   std::default_random_engine random_engine_;
   std::uniform_int_distribution<> dist_;
-  std::array<std::array<Cell, 30>, 30> field_;
+  std::array<std::array<Cell, 5>, 5> field_;
 };
 
 int main()
 {
   LifeField life_field{};
 
-  life_field.show();
+  while (true)
+  {
+    life_field.show();
+    life_field.update();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+  }
   return 0;
 }
