4362c50ac3bf853213b14ec0bf1d097f9b643441
tanacchi <q111026d@mail.kyutech.jp>
2019-04-04 16:22:32 +0900

    Add sample of POST parser with readable stream

---
+++ nodejs/parse_post_with_readstream.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method != 'POST') {
+    res.end();
+    return;
+  }
+
+  let body = '';
+  req.on('data', (chunk) => {  // req is a Readable Stream
+    body += chunk;
+  });
+  req.on('end', () => {
+    console.log(body);
+    res.end();
+  });
+});
+server.listen(3000);
+
+// do this
+// $ curl -X POST -d 'Hello, World!' http://localhost:3000
