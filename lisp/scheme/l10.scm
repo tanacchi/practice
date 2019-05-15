6e2b6bdea4104fc851d234eb6c81b52f7c839934
tanacchi <q111026d@mail.kyutech.jp>
2019-05-16 00:59:11 +0900

    Add question 4 ~ 8

--- lisp/scheme/l10.scm
+++ lisp/scheme/l10.scm
@@ -40,3 +40,82 @@
 
 (print (element-at nums 3))
 (print (element-at chars 3))
+
+
+; #4
+
+(define length
+  (lambda (lst)
+    (cond
+      ((null? lst) 0)
+      (else (+ 1 (length (cdr lst)))))))
+
+(print (length nums))
+(print (length chars))
+
+
+; #5
+
+(define reverse-aux
+  (lambda (lst last)
+    (cond
+      ((null? lst) last)
+      (else (reverse-aux (cdr lst) (cons (car lst) last))))))
+
+(define reverse
+  (lambda (lst)
+    (reverse-aux lst '())))
+
+(print (reverse nums))
+(print (reverse chars))
+
+
+; #6
+
+(define palindrome-test '(0 1 2 3 4 3 2 1 0))
+
+(define list-eq?
+  (lambda (lhs rhs)
+    (cond
+      ((null? lhs) (null? rhs))
+      ((eq? (car lhs) (car rhs)) (list-eq? (cdr lhs) (cdr rhs)))
+      (else #f))))
+
+(define palindrome?
+  (lambda (lst)
+    (cond
+      ((list-eq? (reverse lst) lst) #t)
+      (else #f))))
+
+(print (palindrome? nums))
+(print (palindrome? chars))
+(print (palindrome? palindrome-test))
+
+
+; #7
+
+(define flatten
+  (lambda (lst)
+    (cond
+      ((null? lst) '())
+      ((list? (car lst)) (append (flatten (car lst)) (flatten (cdr lst))))
+      (else (cons (car lst) (flatten (cdr lst)))))))
+
+(print (flatten '(0 1 2 3 4 5 6)))
+(print (flatten '((0 1) (2 (3 4)) 5 (6))))
+
+
+; #8
+
+(define compress
+  (lambda (lst)
+    (cond
+      ((null? lst) '())
+      ((null? (cdr lst)) lst)
+      ((eq? (car lst) (car (cdr lst))) (compress (cdr lst)))
+      (else (cons (car lst) (compress (cdr lst)))))))
+
+(print (compress '()))
+(print (compress '(1 1)))
+(print (compress '(0 1 2 3 4 5 6)))
+(print (compress '(0 0 0 0 1 1 2 2 2 2 2 3 3 3 4 4 4 5 6)))
