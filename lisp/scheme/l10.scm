5b860b09071097834964f4c5b08659a500b486db
tanacchi <q111026d@mail.kyutech.jp>
2019-05-16 02:05:10 +0900

    Add prototype of question 9 and 10

--- lisp/scheme/l10.scm
+++ lisp/scheme/l10.scm
@@ -119,3 +119,13 @@
 (print (compress '(1 1)))
 (print (compress '(0 1 2 3 4 5 6)))
 (print (compress '(0 0 0 0 1 1 2 2 2 2 2 3 3 3 4 4 4 5 6)))
+
+
+; #9
+
+(define pack '())
+
+
+; #10
+
+(define encode '())
