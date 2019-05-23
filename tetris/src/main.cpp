41bea6db0c5ae046090f0c6f9119066ef8f8a16c
tanacchi <q111026d@mail.kyutech.jp>
2019-05-24 01:23:33 +0900

    Add test for pile visualizer

--- tetris/src/main.cpp
+++ tetris/src/main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <tetrimino.hpp>
+#include <pile.hpp>
+#include <main_display.hpp>
+
+#include <unistd.h>  // XXX: sleep function
 
 int main()
 {
-  Tetrimino tetrimino{{
-    {Cell(true),  Cell(true), Cell(false)},
-    {Cell(false), Cell(true), Cell(true)}
-  }};
+  // Tetrimino tetrimino{{
+    // {Cell(true),  Cell(true), Cell(false)},
+    // {Cell(false), Cell(true), Cell(true)}
+  // }};
+  // std::cout << tetrimino.to_string() << std::endl;
 
-  std::cout << tetrimino.to_string() << std::endl;
+  initscr();
+  keypad(stdscr, TRUE);
+  noecho();
+  curs_set(0);
+  refresh();
+
+  MainDisplay disp{};
+  Pile pile{{
+    {Cell(true),  Cell(true),  Cell(true), Cell(true),  Cell(true)},
+    {Cell(true),  Cell(true),  Cell(true), Cell(true),  Cell(true)},
+    {Cell(false), Cell(true),  Cell(true), Cell(true),  Cell(false)},
+    {Cell(false), Cell(true),  Cell(true), Cell(true),  Cell(false)},
+    {Cell(false), Cell(false), Cell(true), Cell(false), Cell(false)},
+  }};
+  disp.show(pile);
+
+  sleep(3);  // XXX: Avoid to use it.
+
+  endwin();
 
   return 0;
 }
