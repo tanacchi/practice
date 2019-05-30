1d81f7f9338ad71789f110a28d8773a6be1f4e01
tanacchi <q111026d@mail.kyutech.jp>
2019-05-30 20:54:29 +0900

    Add l18 function

---
+++ lisp/scheme/l99/l18.scm
@@ -0,0 +1,13 @@
+(define slice-aux
+  (lambda (lst head tail buff)
+    (cond
+      ((null? lst) '())
+      ((> head 1) (slice-aux (cdr lst) (- head 1) (- tail 1) buff))
+      ((zero? tail) buff)
+      (else (append buff (list (car lst)) (slice-aux (cdr lst) 0 (- tail 1) buff))))))
+
+(define slice
+  (lambda (lst head tail)
+    (slice-aux lst head tail '())))
+
+(print (slice '(0 1 2 3) 2 3 ))
