dc376e5e7cc717f7e1fbb667befbadcc24563524
tanacchi <q111026d@mail.kyutech.jp>
2019-07-11 19:12:31 +0900

    Modify function form to parametric

--- other/H29PBL/biotSavart/BiotSavart.cpp
+++ other/H29PBL/biotSavart/BiotSavart.cpp
@@ -13,92 +13,92 @@ constexpr DataType offset_size{0.0001};
 enum index : std::size_t { x = 0, y = 1, z = 2 };
 
 namespace Vector {
-  enum : std::size_t { Demention = 3 };
-  class Vector {
-  public:
-    Vector(DataType x = {}, DataType y = {}, DataType z = {})
-      : elem_{x, y, z}
-    {
-    }
-    const DataType& operator[](int n) const 
-    {
-      return elem_[n];
-    }
-    DataType& operator[](int n)
-    {
-      return elem_[n];
-    }
-    Vector& operator+=(const Vector& rhs)
-    {
-      for (auto i{0u}; i < Demention; ++i) elem_[i] += rhs.elem_[i];
-      return *this;
-    }
-    Vector& operator-=(const Vector& rhs)
-    {
-      for (auto i{0u}; i < Demention; ++i) elem_[i] -= rhs.elem_[i];
-      return *this;
-    }
-    Vector& operator*=(const Vector& rhs)
-    {
-      for (auto i{0u}; i < Demention; ++i) elem_[i] *= rhs.elem_[i];
-      return *this;
-    }
-    Vector& operator/=(const Vector& rhs)
-    {
-      for (auto i{0u}; i < Demention; ++i) elem_[i] /= rhs.elem_[i];
-      return *this;
-    }
-    DataType size() const
-    {
-      DataType sizesq{};
-      for (const auto& e : elem_) sizesq += e*e;
-      return std::sqrt(sizesq);
-    }
-    void show(std::ostream& stream) const
-    {
-      stream << elem_[index::x] << '\t'
-             << elem_[index::y] << '\t'
-             << elem_[index::z] << std::endl;
-    }
-  private:
-    std::array<DataType, Demention> elem_;
-  };
-  Vector operator+(Vector A, const Vector& B)
-  {
-    return A += B;
-  }
-  Vector operator-(Vector A, const Vector& B)
-  {
-    return A -= B;
-  }
-  Vector operator*(Vector A, const Vector& B)
-  {
-    return A *= B;
-  }
-  Vector operator*(DataType k, const Vector& A)
-  {
-    return {k * A[index::x], k * A[index::y], k * A[index::z]};
-  }
-  Vector operator*(const Vector& A, DataType k)
-  {
-    return k * A;
-  }
-  Vector operator/(Vector A, const Vector& B)
-  {
-    return A /= B;
-  }
-  Vector operator/(const Vector& A, DataType k)
-  {
-    return {A[index::x] / k, A[index::y] / k, A[index::z] / k};
-  }
-  Vector cross(const Vector& lhs, const Vector& rhs)
-  {
-    return {
-      lhs[index::y]*rhs[index::z] - lhs[index::z]*rhs[index::y],
-      lhs[index::z]*rhs[index::x] - lhs[index::x]*rhs[index::z],
-      lhs[index::x]*rhs[index::y] - lhs[index::y]*rhs[index::x]
-    };
-  }
+enum : std::size_t { Demention = 3 };
+class Vector {
+ public:
+   Vector(DataType x = {}, DataType y = {}, DataType z = {})
+     : elem_{x, y, z}
+   {
+   }
+   const DataType& operator[](int n) const 
+   {
+     return elem_[n];
+   }
+   DataType& operator[](int n)
+   {
+     return elem_[n];
+   }
+   Vector& operator+=(const Vector& rhs)
+   {
+     for (auto i{0u}; i < Demention; ++i) elem_[i] += rhs.elem_[i];
+     return *this;
+   }
+   Vector& operator-=(const Vector& rhs)
+   {
+     for (auto i{0u}; i < Demention; ++i) elem_[i] -= rhs.elem_[i];
+     return *this;
+   }
+   Vector& operator*=(const Vector& rhs)
+   {
+     for (auto i{0u}; i < Demention; ++i) elem_[i] *= rhs.elem_[i];
+     return *this;
+   }
+   Vector& operator/=(const Vector& rhs)
+   {
+     for (auto i{0u}; i < Demention; ++i) elem_[i] /= rhs.elem_[i];
+     return *this;
+   }
+   DataType size() const
+   {
+     DataType sizesq{};
+     for (const auto& e : elem_) sizesq += e*e;
+     return std::sqrt(sizesq);
+   }
+   void show(std::ostream& stream) const
+   {
+     stream << elem_[index::x] << '\t'
+       << elem_[index::y] << '\t'
+       << elem_[index::z] << std::endl;
+   }
+ private:
+   std::array<DataType, Demention> elem_;
+};
+Vector operator+(Vector A, const Vector& B)
+{
+ return A += B;
+}
+Vector operator-(Vector A, const Vector& B)
+{
+ return A -= B;
+}
+Vector operator*(Vector A, const Vector& B)
+{
+ return A *= B;
+}
+Vector operator*(DataType k, const Vector& A)
+{
+ return {k * A[index::x], k * A[index::y], k * A[index::z]};
+}
+Vector operator*(const Vector& A, DataType k)
+{
+ return k * A;
+}
+Vector operator/(Vector A, const Vector& B)
+{
+ return A /= B;
+}
+Vector operator/(const Vector& A, DataType k)
+{
+ return {A[index::x] / k, A[index::y] / k, A[index::z] / k};
+}
+Vector cross(const Vector& lhs, const Vector& rhs)
+{
+ return {
+   lhs[index::y]*rhs[index::z] - lhs[index::z]*rhs[index::y],
+   lhs[index::z]*rhs[index::x] - lhs[index::x]*rhs[index::z],
+   lhs[index::x]*rhs[index::y] - lhs[index::y]*rhs[index::x]
+ };
+}
 };
 
 struct Domain {
@@ -111,12 +111,12 @@ struct Domain {
 };
 
 struct Route {
-  using FuncType = std::function<DataType(DataType)>;
+  using FuncType = std::function<Vector::Vector(DataType)>;
   Route(FuncType f, Domain d)
     : func{f},
-      domain{d},
-      offset{offset_size * (domain.begin < domain.end ? 1.0 : -1.0)},
-      xnum{(domain.end - domain.begin)/offset}
+    domain{d},
+    offset{offset_size * (domain.begin < domain.end ? 1.0 : -1.0)},
+    xnum{(domain.end - domain.begin)/offset}
   {
   }
   FuncType func;
@@ -129,7 +129,7 @@ struct ElectricCurrent {
   ElectricCurrent(Route r, DataType i = 1.0)
     : route{r}, intensity{i}
   {
-    setPosition(Vector::Vector{route.domain.begin, route.func(route.domain.begin)});
+    setPosition(Vector::Vector{route.func(route.domain.begin)});
     setDirection();
   }
   void setPosition()
@@ -138,8 +138,7 @@ struct ElectricCurrent {
   }
   void setDirection()
   {
-    dir[index::x] = route.offset;
-    dir[index::y] = route.func(pos[index::x] + route.offset) - route.func(pos[index::x]);
+    dir = route.func(pos[index::x] + route.offset) - route.func(pos[index::x]);
   }
   void setPosition(Vector::Vector p)
   {
@@ -173,119 +172,15 @@ Vector::Vector getMagneticVector(const Vector::Vector& r, std::initializer_list<
 
 int main (int argc, char** argv)
 {
-  if (argc != 2) throw std::invalid_argument{"A valid argment is '1', '2' or '3'"};
-  switch (std::atoi(argv[1])) {
-  case 1: // Mission 1
-    {
-      const Route route1{[](DataType x){ return  std::sqrt(1 - x*x); }, {-1.0, 1.0}};
-      const Route route2{[](DataType x){ return -std::sqrt(1 - x*x); }, {1.0, -1.0}};
-      const ElectricCurrent I1{route1, 1.0}; const ElectricCurrent I2{route2, 1.0};
-      std::fstream fstream{"mission1.dat", std::ios_base::out | std::ios_base::trunc};
-      for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.1)
-        for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.1) {
-          Vector::Vector H{getMagneticVector(r, {I1, I2})};
-          fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
-        }
-      fstream << std::endl;
+  const Route route1{[](DataType t){ return Vector::Vector{t,  std::sqrt(1 - t*t), 0}; }, {-1.0, 1.0}};
+  const Route route2{[](DataType t){ return Vector::Vector{t, -std::sqrt(1 - t*t), 0}; }, {1.0, -1.0}};
+  const ElectricCurrent I1{route1, 1.0}; const ElectricCurrent I2{route2, 1.0};
+  std::fstream fstream{"mission1.dat", std::ios_base::out | std::ios_base::trunc};
+  for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.1)
+    for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.1) {
+      Vector::Vector H{getMagneticVector(r, {I1, I2})};
+      fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
     }
-    break;
-  case 2: // Mission 2
-    {
-      constexpr DataType a{1.0};
-      Route route1{[&](DataType x){ return -a*x*x + a; }, {-1.0,  1.0}};
-      Route route2{[&](DataType x){ return  a*x*x - a; }, { 1.0, -1.0}};
-      ElectricCurrent I1{route1, 1.0}; ElectricCurrent I2{route2, 1.0};
-      std::fstream fstream{"mission2.dat", std::ios_base::out | std::ios_base::trunc};
-      for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.1)
-        for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.1) {
-          Vector::Vector H{getMagneticVector(r, {I1, I2})};
-          fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
-        }
-    }
-    break;
-  case 3: // Mission 3
-    {
-      constexpr DataType a{2.0};
-      Route route1{[&](DataType x){ return -a*x*x + a; }, {-1.0,  1.0}};
-      Route route2{[&](DataType x){ return  a*x*x - a; }, { 1.0, -1.0}};
-      ElectricCurrent I1{route1, 1.0}; ElectricCurrent I2{route2, 1.0};
-      {
-        std::fstream fstream{"mission3xy.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{-4.0, -4.0, 0.0}; r[index::x] < 4.0; r[index::x] += 0.15)
-          for (r[index::y] = -4.0; r[index::y] < 4.0; r[index::y] += 0.15) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2})};
-            fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
-          }
-      }
-      {
-        std::fstream fstream{"mission3xz.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{-4.0, 0.0, -4.0}; r[index::x] < 4.0; r[index::x] += 0.15)
-          for (r[index::z] = -4.0; r[index::z] < 4.0; r[index::z] += 0.15) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2})};
-            fstream << r[index::x] << '\t' << H[index::z] << '\t' << r[index::z] << std::endl;
-          }
-      }    
-      {
-        std::fstream fstream{"mission3yz.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{0.0, -4.0, -4.0}; r[index::y] < 4.0; r[index::y] += 0.15)
-          for (r[index::z] = -4.0; r[index::z] < 4.0; r[index::z] += 0.15) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2})};
-            fstream << H[index::x] << '\t' << r[index::y] << '\t' << r[index::z] << std::endl;
-          }
-      }
-    }
-    break;
-  case 4: // Fukuda 2
-    {
-      Route route1{[&](DataType x){ return -x + 1; }, { 0.0,  1.0}};
-      Route route2{[&](DataType x){ return  x + 1; }, {-1.0,  0.0}};
-      Route route3{[&](DataType x){ return -x - 1; }, { 0.0, -1.0}};
-      Route route4{[&](DataType x){ return  x - 1; }, { 1.0,  0.0}};
-      ElectricCurrent I1{route1, 1.0}; ElectricCurrent I2{route2, 1.0};
-      ElectricCurrent I3{route3, 1.0}; ElectricCurrent I4{route4, 1.0};
-      std::fstream fstream{"mission2.dat", std::ios_base::out | std::ios_base::trunc};
-      for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.12)
-        for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.12) {
-          Vector::Vector H{getMagneticVector(r, {I1, I2, I3, I4})};
-          fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
-        }
-    }
-    break;
-  case 5: // Fukuda 3
-    {
-      const Route route1{[](DataType x){ return  std::pow(1 + x*x*x, 0.3333333333); }, {-1.0,  0.0}};
-      const Route route2{[](DataType x){ return  std::pow(1 - x*x*x, 0.3333333333); }, { 0.0,  1.0}};
-      const Route route3{[](DataType x){ return -std::pow(1 + x*x*x, 0.3333333333); }, { 0.0, -1.0}};
-      const Route route4{[](DataType x){ return -std::pow(1 - x*x*x, 0.3333333333); }, { 1.0,  0.0}};
-      ElectricCurrent I1{route1, 1.0}; ElectricCurrent I2{route2, 1.0};
-      ElectricCurrent I3{route3, 1.0}; ElectricCurrent I4{route4, 1.0};
-      {
-        std::fstream fstream{"mission3xy.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{-3.0, -3.0, 0.0}; r[index::x] < 3.0; r[index::x] += 0.12)
-          for (r[index::y] = -3.0; r[index::y] < 3.0; r[index::y] += 0.12) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2, I3, I4})};
-            fstream << r[index::x] << '\t' << r[index::y] << '\t' << H[index::z] << std::endl;
-          }
-      }
-      {
-        std::fstream fstream{"mission3xz.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{-3.0, 0.0, -3.0}; r[index::x] < 3.0; r[index::x] += 0.12)
-          for (r[index::z] = -3.0; r[index::z] < 3.0; r[index::z] += 0.12) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2, I3, I4})};
-            fstream << r[index::x] << '\t' << H[index::z] << '\t' << r[index::z] << std::endl;
-          }
-      }    
-      {
-        std::fstream fstream{"mission3yz.dat", std::ios_base::out | std::ios_base::trunc};
-        for (Vector::Vector r{0.0, -3.0, -3.0}; r[index::y] < 3.0; r[index::y] += 0.12)
-          for (r[index::z] = -3.0; r[index::z] < 3.0; r[index::z] += 0.12) {
-            Vector::Vector H{getMagneticVector(r, {I1, I2, I3, I4})};
-            fstream << H[index::x] << '\t' << r[index::y] << '\t' << r[index::z] << std::endl;
-          }
-      }
-    }
-    break;
-  default: throw std::runtime_error{"A valid argment is '[1..5]'"};
-  }
+  fstream << std::endl;
   return 0;
 }
