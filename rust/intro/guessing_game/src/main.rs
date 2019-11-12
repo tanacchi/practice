967cdebd0888085782070e6d7fcaceb53918556d
tanacchi <q111026d@mail.kyutech.jp>
2019-11-12 13:11:34 +0900

    Complete guessing_game project

--- rust/intro/guessing_game/src/main.rs
+++ rust/intro/guessing_game/src/main.rs
@@ -1,13 +1,34 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Input the number.");
+    let answer = rand::thread_rng().gen_range(1, 101);
+    println!("answer: {}", answer);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-               .expect("Failed to read line.");
+    loop {
+        println!("Input the number.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+                   .expect("Failed to read line.");
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
