48244587a15fcc7c2e3ff16926257cebd91be9ba
tanacchi <q111026d@mail.kyutech.jp>
2019-11-28 20:12:54 +0900

    Use lock on stdin for performance

--- rust/intro/abc/src/main.rs
+++ rust/intro/abc/src/main.rs
@@ -1,9 +1,12 @@
-use std::io;
+use std::io::*;
 
 fn main() {
+    let stdin = stdin();
     loop {
         let mut string_buffer = String::new();
-        io::stdin().read_line(&mut string_buffer).ok();
+        stdin.lock()
+             .read_line(&mut string_buffer)
+             .ok();
         println!("{}", string_buffer);
     }
 }
