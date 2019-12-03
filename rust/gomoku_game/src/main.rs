c44bfa69fb131d83ec51cded216e69ac75113379
tanacchi <q111026d@mail.kyutech.jp>
2019-12-04 02:37:09 +0900

    Complete is_game_finished function
    So dirty.

--- rust/gomoku_game/src/main.rs
+++ rust/gomoku_game/src/main.rs
@@ -18,7 +18,6 @@ impl Stone {
             Stone::Space => ' '
         }
     }
-
 }
 
 fn get_hand(msg: &str) -> usize {
@@ -32,6 +31,7 @@ fn get_hand(msg: &str) -> usize {
 }
 
 fn show_board(board: &[Stone; WIDTH*HEIGHT]) {
+    // chunks can be useful.
     println!("---------------------");
     for i in (0..board.len()).step_by(WIDTH) {
         print!("| ");
@@ -56,37 +56,52 @@ fn insert_stone(board: &mut [Stone; WIDTH*HEIGHT], (x, y): (usize, usize), stone
     }
 }
 
-fn is_game_finished(board: &[Stone; WIDTH*HEIGHT]) -> bool {
-    fn extract_partial_lines(board: &[Stone; WIDTH*HEIGHT], start_i: usize, step: usize) -> [&Stone; 5] {
+fn is_game_finished(board: &[Stone; WIDTH*HEIGHT], active_stone: &Stone) -> bool {
+    fn extract_partial_line(board: &[Stone; WIDTH*HEIGHT], start_i: usize, step: usize) -> [&Stone; 5] {
         let target_line = board[start_i..].iter()
                                           .step_by(step)
                                           .collect::<Vec<_>>();
         // subvector can be useful.
         if target_line.len() >= 5 {
             let mut result_array = [&Stone::Space; 5];
-            result_array.clone_from_slice(&target_line[..]);
+            result_array.clone_from_slice(&target_line[..5]);
             result_array
         } else {
             [&Stone::Space; 5]
         }
     }
 
-    // let horizontal_line = &board[0..20].iter().collect::<Vec<_>>()[0..5];
-    // let vertival_line   = &board[0..20].iter().step_by(WIDTH).collect::<Vec<_>>()[0..5];
-    // let fall_left_line  = &board[0..20].iter().step_by(WIDTH-1);
-    // let fall_right_line = &board[0..20].iter().step_by(WIDTH+1);
-    // println!("horizontal_line:\t{:?}", horizontal_line);
-    // println!("vertival_line:\t\t{:?}", vertival_line);
-    // println!("rising_line:\t\t{:?}", fall_left_line);
-    // println!("falling_line:\t\t{:?}", fall_right_line);
-    true
+    for i in 0..board.len() {
+        let horizontal_line = extract_partial_line(&board, i, 1);
+        let vertival_line   = extract_partial_line(&board, i, WIDTH);
+        let fall_left_line  = extract_partial_line(&board, i, WIDTH-1);
+        let fall_right_line = extract_partial_line(&board, i, WIDTH+1);
+        
+        let is_finished = if horizontal_line.iter().all(|&stone| stone == active_stone) {
+            println!("horizontal_line completed.");
+            true
+        } else if vertival_line.iter().all(|&stone| stone == active_stone) {
+            println!("vertival_line completed.");
+            true
+        } else if fall_left_line.iter().all(|&stone| stone == active_stone) {
+            println!("fall_left_line completed.");
+            true
+        } else if fall_right_line.iter().all(|&stone| stone == active_stone) {
+            println!("fall_right_line completed.");
+            true
+        } else {
+            false
+        };
+        if is_finished { return true };
+    }
+    false
 }
 
 fn main() {
     let mut board = [Stone::Space; WIDTH*HEIGHT];
     let mut active_stone = Stone::White;
 
-    while is_game_finished(&board) {
+    loop {
         let x = get_hand("Input x: ");
         let y = get_hand("Input y: ");
         println!("x = {}, y = {}", x, y);
@@ -94,6 +109,9 @@ fn main() {
         insert_stone(&mut board, (x, y), active_stone);
         show_board(&board);
 
+        if is_game_finished(&board, &active_stone) {
+            return
+        }
         active_stone = match active_stone {
             Stone::White => Stone::Black,
             Stone::Black => Stone::White,
