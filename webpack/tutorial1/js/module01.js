f392ddf8e1ca646e6d4ddc006a230a1b1c4b579e
tanacchi <q111026d@mail.kyutech.jp>
2021-08-10 18:06:39 +0900

    Add base line files for webpack tutorial

---
+++ webpack/tutorial1/js/module01.js
@@ -0,0 +1,2 @@
+var msg_module01 = "module01 stuff";
+console.log(msg_module01);
---
+++ webpack/tutorial1/js/scripts.js
@@ -0,0 +1,6 @@
+require("module01");
+require("module02");
+// var msg_module01 = "module01";
+// console.log(msg_module01);
+// var msg_module02 = "module02";
+// console.log(msg_module02);
