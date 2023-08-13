ddaa39cc2a257dbe539daed63eb9189a8dd4953f
tanacchi <q111026d@mail.kyutech.jp>
2023-08-13 21:10:33 +0900

    Open dev_tools by default

--- tauri/handson1/try-tauri/src-tauri/src/main.rs
+++ tauri/handson1/try-tauri/src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
 use serde::{Deserialize, Serialize};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -58,6 +59,15 @@ fn main() {
             command_with_object,
             command_with_error,
         ])
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
