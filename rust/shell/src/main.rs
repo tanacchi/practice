8ff787927e40570ed6c534574468b760ffd5bbe9
tanacchi <q111026d@mail.kyutech.jp>
2025-02-23 18:45:51 +0900

    2nd step

--- rust/shell/src/main.rs
+++ rust/shell/src/main.rs
@@ -1,12 +1,19 @@
-use std::{io::stdin, process::Command};
+use std::{io::{stdin, stdout, Write}, process::Command};
 
 fn main() {
-  let mut input = String::new();
-  stdin().read_line(& mut input).unwrap();
+  loop {
+    print!("> ");
+    let _ = stdout().flush();
 
-  let command = input.trim();
+    let mut input = String::new();
+    stdin().read_line(& mut input).unwrap();
 
-  Command::new(command)
-    .spawn()
-    .unwrap();
+    let command = input.trim();
+
+    let mut child = Command::new(command)
+      .spawn()
+      .unwrap();
+
+    let _ = child.wait();
+    }
 }
